85e6f9cab5bf0e314dd6eff1927501f02db7649b
BarbarasGitHub <94525038+BarbarasGitHub@users.noreply.github.com>
2021-11-26 14:24:37 +0100

    Add files via upload

---
+++ Mainpage/Mainpage.js
@@ -0,0 +1,20 @@
+// marigold clic
+
+// let marigoldclic = document.getElementById("marigoldclic");
+
+console.log('Babs');
+
+// let appearjava = document.getElementById("appearjava");
+
+// function marigoldclic
+
+    function marigoldclic(){
+        let appearjava = document.getElementById("appearjava");
+
+            // if(getComputedStyle("appearjava").display != "none"){
+                appearjava.style.display = "block";
+            // } else { 
+            //     appearjava.style.display = "block";
+            // }   
+    };
+
